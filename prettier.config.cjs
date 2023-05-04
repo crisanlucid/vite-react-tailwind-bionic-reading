e4e0f80c86e295018a5a611107c7e2946c15f195
Lucian Crisan <crisan.lucid@yahoo.com>
2023-05-04 23:17:26 +0200

    feat: Add EsLint, Prettier config

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+/* eslint-env node */
+module.exports = {
+  endOfLine: 'lf',
+  jsxSingleQuote: true,
+  printWidth: 100,
+  proseWrap: 'never',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+};
