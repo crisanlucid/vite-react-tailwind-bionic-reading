46ef7aeb4977b69aeab91a51dea6486f0ba83d10
Lucian Crisan <crisan.lucid@yahoo.com>
2023-05-04 23:18:11 +0200

    fix: Eslint fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const container = document.getElementById("root");
+if (!container) throw new Error("no container to render to");
+
+const root = createRoot(container);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,40 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  mode: "jit",
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  mode: 'jit',
   theme: {
     fontFamily: {
-      display: ["Open Sans", "sans-serif"],
-      body: ["Open Sans", "sans-serif"],
+      display: ['Open Sans', 'sans-serif'],
+      body: ['Open Sans', 'sans-serif'],
     },
     extend: {
       colors: {
         ...colors,
       },
       screens: {
-        mf: "990px",
+        mf: '990px',
       },
       keyframes: {
-        "slide-in": {
-          "0%": {
-            "-webkit-transform": "translateX(120%)",
-            transform: "translateX(120%)",
+        'slide-in': {
+          '0%': {
+            '-webkit-transform': 'translateX(120%)',
+            transform: 'translateX(120%)',
           },
-          "100%": {
-            "-webkit-transform": "translateX(0%)",
-            transform: "translateX(0%)",
+          '100%': {
+            '-webkit-transform': 'translateX(0%)',
+            transform: 'translateX(0%)',
           },
         },
       },
       animation: {
-        "slide-in": "slide-in 0.5s ease-out",
+        'slide-in': 'slide-in 0.5s ease-out',
       },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: ["@tailwindcss/forms"],
+  plugins: ['@tailwindcss/forms'],
 };
