000484a25d4026527a6b95515c61ae38e3f16b0b
Lucian Crisan <crisan.lucid@yahoo.com>
2023-05-12 08:05:59 +0200

    fix: update config, fix typo

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,39 +1,31 @@
+/* eslint-env node */
 module.exports = {
   extends: [
-    "stylelint-config-standard",
-    "stylelint-config-recess-order",
-    "stylelint-config-css-modules",
-    "stylelint-config-prettier",
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-css-modules',
+    'stylelint-config-prettier',
   ],
-  ignoreFiles: [
-    "./node_modules/**/*.css",
-    "./dist/**/*.css",
-    "./coverage/**/*.css",
-  ],
+  plugins: ['stylelint-scss'],
+  ignoreFiles: ['./node_modules/**/*.css', './dist/**/*.css', './coverage/**/*.css'],
   rules: {
-    "at-rule-no-unknown": [
+    'at-rule-no-unknown': [
       true,
       {
-        ignoreAtRules: [
-          "tailwind",
-          "apply",
-          "screen",
-          "variants",
-          "responsive",
-        ],
+        ignoreAtRules: ['tailwind', 'apply', 'screen', 'variants', 'responsive'],
       },
     ],
-    "no-duplicate-selectors": null,
-    "no-empty-source": null,
-    "rule-empty-line-before": null,
-    "comment-empty-line-before": null,
-    "selector-pseudo-element-no-unknown": null,
-    "declaration-block-trailing-semicolon": null,
-    "no-descending-specificity": null,
-    "string-no-newline": null,
+    'no-duplicate-selectors': null,
+    'no-empty-source': null,
+    'rule-empty-line-before': null,
+    'comment-empty-line-before': null,
+    'selector-pseudo-element-no-unknown': null,
+    'declaration-block-trailing-semicolon': null,
+    'no-descending-specificity': null,
+    'string-no-newline': null,
     // Limit the number of universal selectors in a selector,
     // to avoid very slow selectors
-    "selector-max-universal": 1,
-    "selector-class-pattern": "^[a-z][a-zA-Z0-9]+$",
+    'selector-max-universal': 1,
+    'selector-class-pattern': '^[a-z][a-zA-Z0-9]+$',
   },
 };
