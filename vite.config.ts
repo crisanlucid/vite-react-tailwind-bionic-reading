a3349cbf09c362d3213d79d0cf4dddab9504f027
Lucian Daniel Crisan <crisanlucid@users.noreply.github.com>
2023-12-29 15:37:18 +0000

    feat(): vitest install

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
-import { defineConfig } from "vite";
+/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="vitest" />
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
+// // https://vitejs.dev/config/
+// export default defineConfig({
+  
+//   plugins: [react()],
+// });
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/vite-react-tailwind-bionic-reading/",
-  plugins: [react()],
-});
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    base: "/vite-react-tailwind-bionic-reading/",
+    plugins: [react()],
+    test: {
+      // Do not process css files (is slow)
+      css: false,
+      environment: 'jsdom',
+      // This is to not import test, it, expect, vi (instead of jest). Similar to how jest works
+      globals: true,
+      setupFiles: ["./src/setupTests.ts"],
+    },
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      strictPort: true,
+      port: +process.env?.PORT || 3000,
+    },
+  });
+};
