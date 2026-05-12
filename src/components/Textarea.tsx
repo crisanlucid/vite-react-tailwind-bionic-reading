4123c2c2e3e443a8c7cce7e9faa26f0c28113ded
Lucian Crisan <crisan.lucid@yahoo.com>
2026-05-12 19:50:15 +0200

    feat(components): add Textarea component

---
+++ src/components/Textarea.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+
+interface TextareaProps {
+  onChange: React.ChangeEventHandler<HTMLTextAreaElement>;
+}
+
+export const Textarea: FC<TextareaProps> = ({ onChange }) => (
+  <textarea
+    className='form-control
+    block
+    w-full
+    px-3
+    py-1.5
+    text-base
+    font-normal
+    text-gray-700
+    bg-white bg-clip-padding
+    border border-solid border-gray-300
+    rounded-lg shadow
+    transition
+    ease-in-out
+    m-0
+    mb-4
+    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+    name='text'
+    id='controlTextarea'
+    cols={30}
+    rows={10}
+    aria-label='empty textarea'
+    placeholder='Empty'
+    onChange={onChange}
+  />
+);
