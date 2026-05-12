5fa8d93adfc330dd4cc48c41ef38554bb76741bb
Lucian Crisan <crisan.lucid@yahoo.com>
2026-05-12 20:07:49 +0200

    feat(components): add ReadOutput with forwarded ref

---
+++ src/components/ReadOutput.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface ReadOutputProps {
+  pretext: string;
+  listPrepText: React.ReactElement[];
+}
+
+export const ReadOutput = React.forwardRef<HTMLParagraphElement, ReadOutputProps>(
+  ({ pretext, listPrepText }, ref) => (
+    <p
+      ref={ref}
+      className='whitespace-pre-wrap break-all basis-11/12 shadow mb-4 px-3 py-1.5'
+      id='divToPrint'
+    >
+      {pretext}
+      <span className='t-text'>
+        {listPrepText.map((text, i) => (
+          <React.Fragment key={i}>{text} </React.Fragment>
+        ))}
+      </span>
+    </p>
+  )
+);
+
+ReadOutput.displayName = 'ReadOutput';
